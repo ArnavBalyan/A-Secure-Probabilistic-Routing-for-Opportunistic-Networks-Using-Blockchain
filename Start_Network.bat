38947a0ff7bff13cb279237d2844f87ae6091402
Arnav Balyan <60175178+ArnavBalyan@users.noreply.github.com>
2020-12-15 23:47:56 +0530

    Update Start_Network.bat

--- Start_Network.bat
+++ Start_Network.bat
@@ -1,1 +1,1 @@
-CALL "C:\Program Files\MATLAB\R2019a\bin\matlab.exe" -nodisplay -nosplash - nodesktop -r "Execute; exit"
+CALL "C:\Program Files\MATLAB\R2019a\bin\matlab.exe" -nodisplay -nosplash - nodesktop -r  "Execute; exit"
